51a16c666693859028edd775cf54055696748e8c
Varun Jha <varun.jha@spense.money>
2025-02-22 19:26:07 +0530

    Added supported swift versions

--- Package.swift
+++ Package.swift
@@ -1,24 +1,26 @@
-// swift-tools-version: 5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "sbm-library-ios",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "sbm-library-ios",
-            targets: ["sbm-library-ios"]),
+            targets: ["sbm-library-ios"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "sbm-library-ios"
+            name: "sbm-library-ios",
+            swiftSettings: [
+                .define("SWIFT_VERSION_4_2"),
+                .define("SWIFT_VERSION_5")
+            ]
         ),
         .testTarget(
             name: "sbm-library-iosTests",
-            dependencies: ["sbm-library-ios"]),
-    ]
+            dependencies: ["sbm-library-ios"]
+        ),
+    ],
+    swiftLanguageVersions: [.v4_2, .v5]
 )
